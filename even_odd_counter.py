b43f5f4e71bf162c0021c13076587c791b3251d9
Anjum <anjumkousar079@gmail.com>
2025-10-04 11:21:28 +0530

    Added prime number counting feature

--- even_odd_counter.py
+++ even_odd_counter.py
@@ -1,15 +1,27 @@
-# even_odd_counter.py
+ # even_odd_counter.py
 
 numbers = [10, 21, 4, 45, 66, 93, 1, 7, 13, 2]
 
 even_count = 0
 odd_count = 0
+prime_count = 0
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 for num in numbers:
     if num % 2 == 0:
         even_count += 1
     else:
         odd_count += 1
+    if is_prime(num):
+        prime_count += 1
 
 print("Even numbers:", even_count)
 print("Odd numbers:", odd_count)
+print("Prime numbers:", prime_count)
